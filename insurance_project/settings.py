927ba830d1771d9190c721edb07727d559eea6d0
BeauwattsRe <beauwattsgroup@gmail.com>
2026-01-10 11:24:33 +0800

    Corrected Django settings for production

--- insurance_project/settings.py
+++ insurance_project/settings.py
@@ -84,18 +84,6 @@ DATABASES = {
     )
 }
 
-else:
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_insurance', 
-        'USER': 'admin',   
-        'PASSWORD': 'beauwattsre123', 
-        'HOST': 'localhost',         
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
